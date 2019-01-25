647eac26044ee8fb20c52e77c404baee940a590c
Ryan M. Cosheril <Ryan.M.Cosheril@students.ittralee.ie>
2019-01-25 11:01:46 +0000

    character moving at in an acceptable way now

--- RyanCosherilPG/Assets/Movement.cs
+++ RyanCosherilPG/Assets/Movement.cs
@@ -2,27 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    /// <summary>
+    /// direction character will move
+    /// </summary>
     Vector3 direction;
-    Vector3 playerForward;
-    Vector3 cameraStartPosition;
+    /// <summary>
+    /// speed at which character moves
+    /// </summary>
     float speed;
+    /// <summary>
+    /// speed at which camera turns
+    /// </summary>
+    float cameraTurnSpeed;
     enum States {idle, moving, jumping}
+    /// <summary>
+    /// all possible states of character
+    /// </summary>
     States currentState;
     Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         currentState = States.idle;
-        playerForward = transform.forward;
         speed = 2;
-        cameraStartPosition = Camera.main.transform.position;
+        cameraTurnSpeed = 10;
         //gameObject.AddComponent<Rigidbody>();
         //rb = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        Camera.main.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Horizontal") * cameraTurnSpeed * Time.deltaTime);
+
         switch (currentState)
         {
             case States.idle:
@@ -52,81 +66,135 @@ public class Movement : MonoBehaviour {
                     currentState = States.idle;
                 break;
         }
-
-        Camera.main.transform.position = transform.position + cameraStartPosition;
     }
 
+    /// <summary>
+    /// checks if no movement input is detected and stops moving
+    /// </summary>
     private bool isIdle()
     {
-        if (!shouldMoveForward() && !shouldTurnLeft() && !shouldTurnRight() && !shouldTurnAround())          
+        if (!shouldMoveForward() && !shouldTurnLeft() && !shouldTurnRight() && !shouldTurnAround())
             return true;
-            
+
         else
             return false;
     }
 
-    private void move()
-    {
-        transform.position += speed * direction * Time.deltaTime;
-        currentState = States.moving;
-    }
-
+    /// <summary>
+    /// check if move forward key is pressed
+    /// </summary>
     private bool shouldMoveForward()
     {
         return Input.GetKey("w");
     }
 
+    /// <summary>
+    /// moves character forward
+    /// </summary>
     private void moveForward()
     {
-        direction = transform.forward;
-        move();
+        direction = Camera.main.transform.forward;
+        transform.position += speed * direction * Time.deltaTime;
+        currentState = States.moving;
     }
 
+    /// <summary>
+    /// checks if turn left key is pressed
+    /// </summary>
     private bool shouldTurnLeft()
     {
         return Input.GetKey("a");
     }
 
+    /// <summary>
+    /// turns the character to face left of the camera
+    /// </summary>
     private void turnLeft()
     {
-        direction = -transform.right;
-        if (transform.rotation.y != -90)
-        {
-            Debug.Log("Yas");
-        }
-            transform.Rotate(0,-90,0);
-        move();
+        direction = -Camera.main.transform.right;
+        transform.position += speed * direction * Time.deltaTime;
+        currentState = States.moving;
     }
 
+    /// <summary>
+    /// checks if turn right key is pressed
+    /// </summary>
     private bool shouldTurnRight()
     {
         return Input.GetKey("d");
     }
 
+    /// <summary>
+    /// turns the character to face right of the camera
+    /// </summary>
     private void turnRight()
     {
-        direction = transform.right;
-        move();
+        direction = Camera.main.transform.right;
+        transform.position += speed * direction * Time.deltaTime;
+        currentState = States.moving;
     }
 
+    /// <summary>
+    /// checks if turn around key is pressed
+    /// </summary>
     private bool shouldTurnAround()
     {
         return Input.GetKey("s");
     }
 
+    /// <summary>
+    /// turns the character around to face camera
+    /// </summary>
     private void turnAround()
     {
-        direction = -transform.forward;
-        move();
+        direction = -Camera.main.transform.forward;
+        transform.position += speed * direction * Time.deltaTime;
+        currentState = States.moving;
     }
 
+    /// <summary>
+    /// checks if jump key is pressed
+    /// </summary>
     private bool shouldJump()
     {
         return Input.GetKeyDown(KeyCode.Space);
     }
 
+    /// <summary>
+    /// makes the character jump
+    /// </summary>
     private void jump()
     {
 
+    }
+
+    /// <summary>
+    /// attacks with melee weapon
+    /// </summary>
+    private void meleeAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// attacks with ranged weapon
+    /// </summary>
+    private void rangedAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// checks if any interactable objects can be interacted with
+    /// </summary>
+    private bool canInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <param name="interactedObject">object interacted with</param>
+    private void interact(GameObject interactedObject)
+    {
+        throw new System.NotImplementedException();
     }
 }
